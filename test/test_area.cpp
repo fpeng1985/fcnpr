1da0ca3d76f9b8ae8e18f83d5dcbd1c27e5cd9bf
Ruan <formigoni@outlook.com>
2019-03-10 20:13:49 -0300

    catch2 library integration

--- test/test_area.cpp
+++ test/test_area.cpp
@@ -1,24 +1,62 @@
+#define CATCH_CONFIG_MAIN
+
+#include <catch.hpp>
 #include <Area.hpp>
 
-int main(){
-	using namespace pandr::area;
+using namespace pandr::area;
+using namespace Catch;
+
+
+TEST_CASE("Area.hpp testing" "[area]"){
 	Area area;
+	REQUIRE(area.get() == 0);
 
-	area.set(0,0);
-	area.set(3,3);
-	if(area.get() != 16) return EXIT_FAILURE;
+	SECTION("Area 4x4 testing"){
+		area.set(0,0);
+		area.set(3,3);
+		REQUIRE(area.get() == 16);
+	}
 
-	area.unset(0,0);
-	area.unset(3,3);
-	if(area.get() != 0) return EXIT_FAILURE;
+	SECTION("Area unset testing"){
+		area.set(0,0);
+		area.set(3,3);
+		area.unset(0,0);
+		area.unset(3,3);
+		REQUIRE(area.get() == 0);
+		
+	}
 
-	area.unset(3,1);
-	area.unset(2,2);
-	if(area.get() != 0) return EXIT_FAILURE;
+	SECTION("Area unset testing"){
+		area.set(0,0);
+		area.set(3,3);
+		area.unset(0,0);
+		area.unset(3,3);
+		REQUIRE(area.get() == 0);
+		
+	}
 
-	area.set(5,2);
-	area.set(16,21);
-	if(area.get() != 240) return EXIT_FAILURE;
+	SECTION("Area 12x20 testing"){
+		area.set(5,2);
+		area.set(16,21);
+		REQUIRE(area.get() == 240);
+	}
 
-	return EXIT_SUCCESS;
+	SECTION("Negative values testing"){
+		/* The signs should be ignored */
+		area.set(-1,2);
+		area.set(16,-1);
+		REQUIRE(area.get() == 32);
+
+		area.unset(-1,2);
+		area.unset(16,-1);
+		REQUIRE(area.get() == 0);
+
+		area.set(1,-2);
+		area.set(-16,1);
+		REQUIRE(area.get() == 32);
+
+		area.unset(1,-2);
+		area.unset(-16,1);
+		REQUIRE(area.get() == 0);
+	}
 }
